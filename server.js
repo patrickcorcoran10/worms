3ca24d0a9cdb02204f6701d1955e43d446a8f0ff
Patty C <patrickcorcoran10@gmail.com>
2020-04-03 16:05:44 -0500

    worms game running. need to add keep score

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(PORT, function() {
+  console.log("We Got Worms on PORT", PORT);
+});
